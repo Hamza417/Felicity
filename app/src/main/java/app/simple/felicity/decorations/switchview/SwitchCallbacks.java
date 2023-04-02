9181a282e24edae165d006ba8702a441b4f75cd7
Hamza417 <HamzaRizwan243@gmail.com>
2023-04-02 19:35:47 +0530

    Initial Commit
      - Setup switch framework

---
+++ app/src/main/java/app/simple/felicity/decorations/switchview/SwitchCallbacks.java
@@ -0,0 +1,5 @@
+package app.simple.felicity.decorations.switchview;
+
+public interface SwitchCallbacks {
+    void onCheckedChanged(boolean isChecked);
+}
---
+++ app/src/main/java/app/simple/felicity/preferences/ConfigurationPreferences.kt
@@ -0,0 +1,62 @@
+package app.simple.felicity.preferences
+
+object ConfigurationPreferences {
+
+    private const val keepScreenOn = "keep_screen_on"
+    const val isUsingRoot = "is_using_root"
+    const val isUsingShizuku = "is_using_shizuku"
+    const val language = "language_of_app"
+    const val appPath = "app_path"
+
+    fun setKeepScreenOn(value: Boolean) {
+        SharedPreferences.getSharedPreferences().edit().putBoolean(keepScreenOn, value).apply()
+    }
+
+    fun isKeepScreenOn(): Boolean {
+        return SharedPreferences.getSharedPreferences().getBoolean(keepScreenOn, false)
+    }
+
+    // ---------------------------------------------------------------------------------------------------------- //
+
+    fun setUsingRoot(value: Boolean) {
+        SharedPreferences.getSharedPreferences().edit().putBoolean(isUsingRoot, value).apply()
+    }
+
+    fun isUsingRoot(): Boolean {
+        return SharedPreferences.getSharedPreferences().getBoolean(isUsingRoot, false)
+    }
+
+    // ---------------------------------------------------------------------------------------------------------- //
+
+    fun setAppLanguage(value: String) {
+        SharedPreferences.getSharedPreferences().edit().putString(language, value).apply()
+    }
+
+    fun getAppLanguage(): String? {
+        return SharedPreferences.getSharedPreferences().getString(language, "default")
+    }
+
+    // ---------------------------------------------------------------------------------------------------------- //
+
+    fun setUsingShizuku(value: Boolean) {
+        SharedPreferences.getSharedPreferences().edit().putBoolean(isUsingShizuku, value).apply()
+    }
+
+    fun isUsingShizuku(): Boolean {
+        return SharedPreferences.getSharedPreferences().getBoolean(isUsingShizuku, false)
+    }
+
+    // ---------------------------------------------------------------------------------------------------------- //
+
+    fun setAppPath(value: String) {
+        SharedPreferences.getSharedPreferences().edit().putString(appPath, value).apply()
+    }
+
+    fun getAppPath(): String {
+        return SharedPreferences.getSharedPreferences().getString(appPath, "Inure App Manager") ?: "Inure App Manager"
+    }
+
+    fun defaultAppPath() {
+        SharedPreferences.getSharedPreferences().edit().putString(appPath, "Inure App Manager").apply()
+    }
+}
