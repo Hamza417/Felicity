5b722d8680580ea870a59f756110f7151b67fa07
Hamza417 <HamzaRizwan243@gmail.com>
2023-05-29 02:34:04 +0530

    Initial Commit
      - Added tags

--- app/src/main/java/app/simple/felicity/ui/app/ArtFlow.kt
+++ app/src/main/java/app/simple/felicity/ui/app/ArtFlow.kt
@@ -9,20 +9,20 @@ import androidx.lifecycle.ViewModelProvider
 import app.simple.felicity.R
 import app.simple.felicity.adapters.SongsAdapter
 import app.simple.felicity.constants.BundleConstants
+import app.simple.felicity.databinding.FragmentArtflowBinding
 import app.simple.felicity.decorations.carousel.ArtFlowCarousel
 import app.simple.felicity.viewmodels.ui.SongsViewModel
 import app.simple.inure.extensions.fragments.ScopedFragment
 
 class ArtFlow : ScopedFragment() {
 
-    private lateinit var coverFlow: ArtFlowCarousel
+    private var binding: FragmentArtflowBinding? = null
     private lateinit var songsViewModel: SongsViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_artflow, container, false)
 
-        coverFlow = view.findViewById(R.id.artflow)
-
+        binding = FragmentArtflowBinding.bind(view)
         songsViewModel = ViewModelProvider(requireActivity())[SongsViewModel::class.java]
 
         return view
@@ -33,9 +33,9 @@ class ArtFlow : ScopedFragment() {
         startPostponedEnterTransition()
 
         songsViewModel.getSongs().observe(viewLifecycleOwner) {
-            coverFlow.adapter = SongsAdapter(requireContext(), it)
+            binding?.artflow?.adapter = SongsAdapter(requireContext(), it)
 
-            coverFlow.setOnScrollPositionListener(object : ArtFlowCarousel.OnScrollPositionListener {
+            binding?.artflow?.setOnScrollPositionListener(object : ArtFlowCarousel.OnScrollPositionListener {
                 override fun onScrolledToPosition(position: Int) {
                     Log.d("ArtFlow", "Scrolled to position: $position")
                 }
@@ -45,20 +45,16 @@ class ArtFlow : ScopedFragment() {
                 }
             })
 
-            coverFlow.setOnItemSelectedListener { child, position ->
+            binding?.artflow?.setOnItemSelectedListener { child, position ->
                 Log.d("ArtFlow", "Selected item: $position")
                 requireArguments().putInt(BundleConstants.position, position)
+
+                binding?.title?.text = it[position].title
+                binding?.artist?.text = it[position].artist
             }
 
-            coverFlow.scrollToPosition(requireArguments().getInt(BundleConstants.position, 0))
+            binding?.artflow?.scrollToPosition(requireArguments().getInt(BundleConstants.position, 0))
         }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        Log.d("ArtFlow", "Saving state")
-        Log.d("ArtFlow", "Current position: ${coverFlow.currentItemPosition}")
-        outState.putInt(BundleConstants.position, coverFlow.currentItemPosition)
-        super.onSaveInstanceState(outState)
     }
 
     companion object {
