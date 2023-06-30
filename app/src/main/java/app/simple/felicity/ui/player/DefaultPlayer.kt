214c6e0adc9d71787309a149c3bffda5d8906794
Hamza417 <HamzaRizwan243@gmail.com>
2023-06-30 23:21:21 +0530

    Initial Commit
      - Transparent system bars framework

---
+++ app/src/main/java/app/simple/felicity/ui/player/DefaultPlayer.kt
@@ -0,0 +1,14 @@
+package app.simple.felicity.ui.player
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.simple.inure.extensions.fragments.ScopedFragment
+
+class DefaultPlayer : ScopedFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
