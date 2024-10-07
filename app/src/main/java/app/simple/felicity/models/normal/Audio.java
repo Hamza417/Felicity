5f7db1eeddb07b7ddbc39c568d778bb3891c1015
Hamza417 <HamzaRizwan243@gmail.com>
2024-10-07 11:46:35 +0530

    Update Audio.java

--- app/src/main/java/app/simple/felicity/models/normal/Audio.java
+++ app/src/main/java/app/simple/felicity/models/normal/Audio.java
@@ -3,6 +3,7 @@ package app.simple.felicity.models.normal;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
@@ -379,11 +380,180 @@ public class Audio implements Parcelable {
         this.bitPerSample = bitPerSample;
     }
     
+    @Nullable
     public String getWriter() {
         return writer;
     }
     
-    public void setWriter(String writer) {
+    public void setWriter(@Nullable String writer) {
         this.writer = writer;
+    }
+    
+    @NonNull
+    @Override
+    public String toString() {
+        return "Audio{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                ", album='" + album + '\'' +
+                ", path='" + path + '\'' +
+                ", track=" + track +
+                ", author='" + author + '\'' +
+                ", size=" + size +
+                ", albumArtist='" + albumArtist + '\'' +
+                ", year='" + year + '\'' +
+                ", bitrate=" + bitrate +
+                ", duration=" + duration +
+                ", composer='" + composer + '\'' +
+                ", date='" + date + '\'' +
+                ", discNumber='" + discNumber + '\'' +
+                ", genre='" + genre + '\'' +
+                ", dateAdded=" + dateAdded +
+                ", dateModified=" + dateModified +
+                ", dateTaken=" + dateTaken +
+                ", albumId=" + albumId +
+                ", trackNumber='" + trackNumber + '\'' +
+                ", compilation='" + compilation + '\'' +
+                ", mimeType='" + mimeType + '\'' +
+                ", numTracks='" + numTracks + '\'' +
+                ", samplingRate=" + samplingRate +
+                ", bitPerSample=" + bitPerSample +
+                ", writer='" + writer + '\'' +
+                '}';
+    }
+    
+    /**
+     * @noinspection EqualsReplaceableByObjectsCall
+     */
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        Audio audio = (Audio) obj;
+        
+        if (id != audio.id) {
+            return false;
+        }
+        if (track != audio.track) {
+            return false;
+        }
+        if (size != audio.size) {
+            return false;
+        }
+        if (bitrate != audio.bitrate) {
+            return false;
+        }
+        if (duration != audio.duration) {
+            return false;
+        }
+        if (dateAdded != audio.dateAdded) {
+            return false;
+        }
+        if (dateModified != audio.dateModified) {
+            return false;
+        }
+        if (dateTaken != audio.dateTaken) {
+            return false;
+        }
+        if (albumId != audio.albumId) {
+            return false;
+        }
+        if (samplingRate != audio.samplingRate) {
+            return false;
+        }
+        if (bitPerSample != audio.bitPerSample) {
+            return false;
+        }
+        if (name != null ? !name.equals(audio.name) : audio.name != null) {
+            return false;
+        }
+        if (title != null ? !title.equals(audio.title) : audio.title != null) {
+            return false;
+        }
+        if (artist != null ? !artist.equals(audio.artist) : audio.artist != null) {
+            return false;
+        }
+        if (album != null ? !album.equals(audio.album) : audio.album != null) {
+            return false;
+        }
+        if (path != null ? !path.equals(audio.path) : audio.path != null) {
+            return false;
+        }
+        if (author != null ? !author.equals(audio.author) : audio.author != null) {
+            return false;
+        }
+        if (albumArtist != null ? !albumArtist.equals(audio.albumArtist) : audio.albumArtist != null) {
+            return false;
+        }
+        if (year != null ? !year.equals(audio.year) : audio.year != null) {
+            return false;
+        }
+        if (composer != null ? !composer.equals(audio.composer) : audio.composer != null) {
+            return false;
+        }
+        if (date != null ? !date.equals(audio.date) : audio.date != null) {
+            return false;
+        }
+        if (discNumber != null ? !discNumber.equals(audio.discNumber) : audio.discNumber != null) {
+            return false;
+        }
+        if (genre != null ? !genre.equals(audio.genre) : audio.genre != null) {
+            return false;
+        }
+        if (trackNumber != null ? !trackNumber.equals(audio.trackNumber) : audio.trackNumber != null) {
+            return false;
+        }
+        if (compilation != null ? !compilation
+                .equals(audio.compilation) : audio.compilation != null) {
+            return false;
+        }
+        if (mimeType != null ? !mimeType.equals(audio.mimeType) : audio.mimeType != null) {
+            return false;
+        }
+        if (numTracks != null ? !numTracks.equals(audio.numTracks) : audio.numTracks != null) {
+            return false;
+        }
+        return writer != null ? writer.equals(audio.writer) : audio.writer == null;
+    }
+    
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (int) (id ^ (id >>> 32));
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (artist != null ? artist.hashCode() : 0);
+        result = 31 * result + (album != null ? album.hashCode() : 0);
+        result = 31 * result + (path != null ? path.hashCode() : 0);
+        result = 31 * result + track;
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        result = 31 * result + size;
+        result = 31 * result + (albumArtist != null ? albumArtist.hashCode() : 0);
+        result = 31 * result + (year != null ? year.hashCode() : 0);
+        result = 31 * result + (int) (bitrate ^ (bitrate >>> 32));
+        result = 31 * result + (int) (duration ^ (duration >>> 32));
+        result = 31 * result + (composer != null ? composer.hashCode() : 0);
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        result = 31 * result + (discNumber != null ? discNumber.hashCode() : 0);
+        result = 31 * result + (genre != null ? genre.hashCode() : 0);
+        result = 31 * result + (int) (dateAdded ^ (dateAdded >>> 32));
+        result = 31 * result + (int) (dateModified ^ (dateModified >>> 32));
+        result = 31 * result + (int) (dateTaken ^ (dateTaken >>> 32));
+        result = 31 * result + (int) (albumId ^ (albumId >>> 32));
+        result = 31 * result + (trackNumber != null ? trackNumber.hashCode() : 0);
+        result = 31 * result + (compilation != null ? compilation.hashCode() : 0);
+        result = 31 * result + (mimeType != null ? mimeType.hashCode() : 0);
+        result = 31 * result + (numTracks != null ? numTracks.hashCode() :
+                0);
+        result = 31 * result + (int) (samplingRate ^ (samplingRate >>> 32));
+        result = 31 * result + (int) (bitPerSample ^ (bitPerSample >>> 32));
+        result = 31 * result + (writer != null ? writer.hashCode() : 0);
+        return result;
     }
 }
