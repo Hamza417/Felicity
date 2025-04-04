3119c444237c237b8f9238777b5d407cd69c4c56
Hamza417 <23103729+Hamza417@users.noreply.github.com>
2025-04-04 10:59:43 +0530

    fix: art flow home data issue

--- music/src/main/java/app/simple/felicity/adapters/home/sub/ArtFlowAdapter.kt
+++ music/src/main/java/app/simple/felicity/adapters/home/sub/ArtFlowAdapter.kt
@@ -1,13 +1,17 @@
 package app.simple.felicity.adapters.home.sub
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import app.simple.felicity.R
 import app.simple.felicity.decorations.typeface.TypeFaceTextView
 import app.simple.felicity.decorations.views.SquareImageView
+import app.simple.felicity.glide.pathcover.PathCoverModel
 import app.simple.felicity.glide.utils.HomeUtils
 import app.simple.felicity.repository.models.home.Home
+import app.simple.felicity.repository.models.home.HomeAudio
+import com.bumptech.glide.Glide
 import com.smarteist.autoimageslider.SliderViewAdapter
 
 class ArtFlowAdapter(private val data: Home) : SliderViewAdapter<ArtFlowAdapter.ArtFlowViewHolder>() {
@@ -16,6 +20,10 @@ class ArtFlowAdapter(private val data: Home) : SliderViewAdapter<ArtFlowAdapter.
         val art: SquareImageView = itemView.findViewById(R.id.art)
         val title: TypeFaceTextView = itemView.findViewById(R.id.title)
         val artist: TypeFaceTextView = itemView.findViewById(R.id.artist)
+
+        fun getContext(): Context {
+            return itemView.context
+        }
     }
 
     override fun getCount(): Int {
@@ -27,6 +35,11 @@ class ArtFlowAdapter(private val data: Home) : SliderViewAdapter<ArtFlowAdapter.
     }
 
     override fun onBindViewHolder(viewHolder: ArtFlowViewHolder, position: Int) {
-        HomeUtils.loadHomeAlbumArt(data, viewHolder.art, position)
+        Glide.with(viewHolder.art)
+            .asBitmap()
+            .load(PathCoverModel(viewHolder.getContext(), (data as HomeAudio).audios[position].path))
+            .dontTransform()
+            .dontAnimate()
+            .into(viewHolder.art)
     }
 }
--- music/src/main/java/app/simple/felicity/viewmodels/main/home/HomeViewModel.kt
+++ music/src/main/java/app/simple/felicity/viewmodels/main/home/HomeViewModel.kt
@@ -5,16 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import app.simple.felicity.R
 import app.simple.felicity.extensions.viewmodels.WrappedViewModel
-import app.simple.felicity.repository.loaders.MediaStoreLoader.loadAlbums
-import app.simple.felicity.repository.loaders.MediaStoreLoader.loadArtists
-import app.simple.felicity.repository.loaders.MediaStoreLoader.loadAudios
+import app.simple.felicity.repository.database.instances.AudioDatabase
 import app.simple.felicity.repository.models.home.Home
-import app.simple.felicity.repository.models.home.HomeAlbum
-import app.simple.felicity.repository.models.home.HomeArtist
 import app.simple.felicity.repository.models.home.HomeAudio
-import app.simple.felicity.repository.models.home.HomeBookmarks
-import app.simple.felicity.repository.models.home.HomeLinks
-import app.simple.felicity.utils.ArrayUtils.toArrayList
+import app.simple.felicity.repository.models.normal.Audio
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -32,23 +26,30 @@ class HomeViewModel(application: Application) : WrappedViewModel(application) {
 
     private fun loadData() {
         viewModelScope.launch(Dispatchers.IO) {
-            val songs = applicationContext().loadAudios()
-            val albums = applicationContext().loadAlbums()
-            val artists = applicationContext().loadArtists()
-            val recentlyAdded = applicationContext().loadAudios("date_added DESC").toArrayList()
+            val audioDatabase = AudioDatabase.getInstance(applicationContext())
+            val songs = audioDatabase?.audioDao()?.getAllAudio()?.take(TAKE_COUNT) as ArrayList<Audio>
+            val albums = audioDatabase.audioDao()?.getAllAlbums()?.take(TAKE_COUNT) as ArrayList<Audio>
+            val artists = audioDatabase.audioDao()?.getAllArtists()?.take(TAKE_COUNT) as ArrayList<Audio>
+            val recentlyAdded = audioDatabase.audioDao()?.getRecentAudio()?.take(TAKE_COUNT) as ArrayList<Audio>
 
             val homeData = arrayListOf<Home>()
 
             homeData.add(HomeAudio(R.string.songs, R.drawable.ic_music, songs))
-            homeData.add(HomeAlbum(R.string.albums, R.drawable.ic_album, albums))
-            homeData.add(HomeArtist(R.string.artists, R.drawable.ic_artist, artists))
+            homeData.add(HomeAudio(R.string.albums, R.drawable.ic_album, albums))
+            homeData.add(HomeAudio(R.string.artists, R.drawable.ic_artist, artists))
             homeData.add(HomeAudio(R.string.recently_added, R.drawable.ic_history, recentlyAdded))
-            homeData.add(HomeAudio(R.string.folders, R.drawable.ic_folder, arrayListOf()))
-            homeData.add(HomeAudio(R.string.favorites, R.drawable.ic_favorite, arrayListOf()))
-            homeData.add(HomeLinks(R.string.links, R.drawable.ic_link, 0))
-            homeData.add(HomeBookmarks(R.string.bookmarks, R.drawable.ic_bookmarks, 0))
+
+            // homeData.add(HomeAudio(R.string.folders, R.drawable.ic_folder, arrayListOf()))
+            // homeData.add(HomeAudio(R.string.favorites, R.drawable.ic_favorite, arrayListOf()))
+            // homeData.add(HomeLinks(R.string.links, R.drawable.ic_link, 0))
+            // homeData.add(HomeBookmarks(R.string.bookmarks, R.drawable.ic_bookmarks, 0))
 
             data.postValue(homeData)
         }
+    }
+
+    companion object {
+        const val TAG = "HomeViewModel"
+        private const val TAKE_COUNT = 18
     }
 }
