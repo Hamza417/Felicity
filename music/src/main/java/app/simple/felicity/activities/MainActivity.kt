87571d2d7c5693842d87628c8e9f131d8c1c6a71
Hamza417 <23103729+Hamza417@users.noreply.github.com>
2025-08-19 23:13:50 +0530

    remove: instant fragment change logic
    // I dont think this is a good UX

--- music/src/main/java/app/simple/felicity/activities/MainActivity.kt
+++ music/src/main/java/app/simple/felicity/activities/MainActivity.kt
@@ -1,21 +1,16 @@
 package app.simple.felicity.activities
 
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import android.view.KeyEvent
 import app.simple.felicity.R
 import app.simple.felicity.dialogs.app.VolumeKnob.Companion.showVolumeKnob
 import app.simple.felicity.extensions.activities.BaseActivity
 import app.simple.felicity.preferences.HomePreferences
-import app.simple.felicity.preferences.SongsPreferences
 import app.simple.felicity.shared.utils.ConditionUtils.isNull
-import app.simple.felicity.ui.app.CoverFlow
 import app.simple.felicity.ui.main.home.ArtFlowHome
 import app.simple.felicity.ui.main.home.CarouselHome
 import app.simple.felicity.ui.main.home.SimpleHome
 import app.simple.felicity.ui.main.home.SpannedHome
-import app.simple.felicity.ui.main.songs.Songs
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -34,15 +29,6 @@ class MainActivity : BaseActivity() {
     }
 
     private fun setHomePanel() {
-        val tags = listOf(
-                CarouselHome.TAG,
-                SpannedHome.TAG,
-                ArtFlowHome.TAG,
-                SimpleHome.TAG
-        )
-
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.app_container)
-
         when (HomePreferences.getHomeInterface()) {
             HomePreferences.HOME_INTERFACE_CAROUSEL -> {
                 supportFragmentManager.beginTransaction()
@@ -68,35 +54,6 @@ class MainActivity : BaseActivity() {
         }
     }
 
-    private fun updateSongsPanel() {
-        val tags = listOf(
-                Songs.TAG,
-                CoverFlow.TAG
-        )
-
-        tags.forEach {
-            val fragment = supportFragmentManager.findFragmentByTag(it)
-            if (fragment != null) {
-                Log.d("MainActivity", "Removing fragment: $it")
-                supportFragmentManager.beginTransaction().remove(fragment).commitAllowingStateLoss()
-            }
-        }
-
-        when (SongsPreferences.getSongsInterface()) {
-            SongsPreferences.SONG_INTERFACE_FELICITY -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.app_container, Songs.newInstance(), Songs.TAG)
-                    .commit()
-            }
-
-            SongsPreferences.SONG_INTERFACE_FLOW -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.app_container, CoverFlow.newInstance(), CoverFlow.TAG)
-                    .commit()
-            }
-        }
-    }
-
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         return when (keyCode) {
             KeyEvent.KEYCODE_VOLUME_UP -> {
@@ -111,18 +68,6 @@ class MainActivity : BaseActivity() {
 
             else -> {
                 super.onKeyDown(keyCode, event)
-            }
-        }
-    }
-
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
-        super.onSharedPreferenceChanged(sharedPreferences, key)
-        when (key) {
-            HomePreferences.HOME_INTERFACE -> {
-                setHomePanel()
-            }
-            SongsPreferences.SONGS_INTERFACE -> {
-                updateSongsPanel()
             }
         }
     }
