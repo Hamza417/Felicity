35a63aaaf407378c7e453894d12d028150e55e23
Hamza417 <23103729+Hamza417@users.noreply.github.com>
2025-08-30 05:36:43 +0530

    update: genre ui

--- music/src/main/java/app/simple/felicity/adapters/ui/lists/genres/AdapterGenres.kt
+++ music/src/main/java/app/simple/felicity/adapters/ui/lists/genres/AdapterGenres.kt
@@ -4,74 +4,34 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import app.simple.felicity.callbacks.GeneralAdapterCallbacks
 import app.simple.felicity.databinding.AdapterGenresBinding
-import app.simple.felicity.databinding.AdapterHeaderGenresBinding
 import app.simple.felicity.decorations.overscroll.VerticalListViewHolder
-import app.simple.felicity.decorations.utils.RecyclerViewUtils
 import app.simple.felicity.glide.genres.GenreCoverUtils.loadGenreCover
 import app.simple.felicity.repository.models.Genre
 
-class AdapterGenres(private val list: List<Genre>) : androidx.recyclerview.widget.RecyclerView.Adapter<VerticalListViewHolder>() {
+class AdapterGenres(private val list: List<Genre>) : androidx.recyclerview.widget.RecyclerView.Adapter<AdapterGenres.Holder>() {
 
     private var callbacks: GeneralAdapterCallbacks? = null
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VerticalListViewHolder {
-        return when (viewType) {
-            RecyclerViewUtils.TYPE_HEADER -> {
-                Header(
-                        AdapterHeaderGenresBinding.inflate(
-                                LayoutInflater.from(parent.context),
-                                parent,
-                                false
-                        )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        return Holder(
+                AdapterGenresBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
                 )
-            }
-            else -> {
-                Holder(
-                        AdapterGenresBinding.inflate(
-                                LayoutInflater.from(parent.context),
-                                parent,
-                                false
-                        )
-                )
-            }
-        }
+        )
     }
 
-    override fun onBindViewHolder(holder: VerticalListViewHolder, position: Int) {
-        if (holder is Header) {
-            // No binding needed for header
-        } else if (holder is Holder) {
-            val genre = list[position - 1] // Adjust for header
-            holder.bind(genre)
-        }
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(list[position])
     }
 
     override fun getItemId(position: Int): Long {
-        return if (position == 0) {
-            RecyclerViewUtils.TYPE_HEADER.toLong()
-        } else {
-            list[position - 1].id // Adjust for header
-        }
+        return list[position].id
     }
 
     override fun getItemCount(): Int {
-        return list.size.plus(1)
-    }
-
-    override fun getItemViewType(position: Int): Int {
-        return if (position == 0) {
-            RecyclerViewUtils.TYPE_HEADER
-        } else {
-            RecyclerViewUtils.TYPE_ITEM
-        }
-    }
-
-    inner class Header(private val binding: AdapterHeaderGenresBinding) : VerticalListViewHolder(binding.root) {
-        init {
-            binding.menu.setOnClickListener {
-                callbacks?.onMenuClicked(it)
-            }
-        }
+        return list.size
     }
 
     inner class Holder(private val binding: AdapterGenresBinding) : VerticalListViewHolder(binding.root) {
--- music/src/main/java/app/simple/felicity/ui/main/genres/Genres.kt
+++ music/src/main/java/app/simple/felicity/ui/main/genres/Genres.kt
@@ -8,14 +8,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.view.forEach
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.transition.TransitionManager
 import app.simple.felicity.R
 import app.simple.felicity.adapters.ui.lists.genres.AdapterGenres
 import app.simple.felicity.callbacks.GeneralAdapterCallbacks
 import app.simple.felicity.databinding.FragmentGenresBinding
+import app.simple.felicity.databinding.HeaderGenresBinding
+import app.simple.felicity.decorations.fastscroll.SlideFastScroller
 import app.simple.felicity.decorations.itemdecorations.GridSpacingItemDecoration
+import app.simple.felicity.decorations.views.AppHeader
 import app.simple.felicity.dialogs.genres.DialogGenreMenu.Companion.showGenreMenu
 import app.simple.felicity.extensions.fragments.ScopedFragment
 import app.simple.felicity.preferences.GenresPreferences
@@ -25,23 +28,29 @@ import com.bumptech.glide.Glide
 
 class Genres : ScopedFragment() {
 
-    private var genresViewModel: GenresViewModel? = null
+    private val genresViewModel: GenresViewModel by viewModels({ requireActivity() })
 
     private lateinit var binding: FragmentGenresBinding
+    private lateinit var headerBinding: HeaderGenresBinding
+
     private lateinit var gridLayoutManager: GridLayoutManager
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         binding = FragmentGenresBinding.inflate(inflater, container, false)
-        genresViewModel = ViewModelProvider(requireActivity())[GenresViewModel::class.java]
+        headerBinding = HeaderGenresBinding.inflate(inflater, container, false)
 
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.header.setContentView(headerBinding.root)
+        binding.header.attachTo(binding.recyclerView, AppHeader.ScrollMode.HIDE_ON_SCROLL)
+        SlideFastScroller.attach(binding.recyclerView)
+        postponeEnterTransition()
 
-        genresViewModel!!.getGenresData().observe(viewLifecycleOwner) { genres ->
+        genresViewModel.getGenresData().observe(viewLifecycleOwner) { genres ->
             Log.d(TAG, "onViewCreated: Genres: ${genres.size}")
             val adapter = AdapterGenres(genres)
             adapter.setHasStableIds(true)
@@ -49,22 +58,12 @@ class Genres : ScopedFragment() {
             gridLayoutManager = GridLayoutManager(requireContext(), GenresPreferences.getGridSize())
             binding.recyclerView.layoutManager = gridLayoutManager
 
-            gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
-                override fun getSpanSize(position: Int): Int {
-                    return if (position == 0) {
-                        gridLayoutManager.spanCount
-                    } else {
-                        1
-                    }
-                }
-            }
-
             binding.recyclerView.addItemDecoration(GridSpacingItemDecoration(
                     gridLayoutManager.spanCount,
                     resources.getDimensionPixelOffset(R.dimen.padding_15),
                     resources.getDimensionPixelOffset(R.dimen.padding_15),
                     true,
-                    1))
+                    0))
             binding.recyclerView.setHasFixedSize(true)
             binding.recyclerView.adapter = adapter
 
@@ -78,6 +77,12 @@ class Genres : ScopedFragment() {
                     openFragment(GenrePage.newInstance(genre), GenrePage.TAG)
                 }
             })
+
+            headerBinding.menu.setOnClickListener {
+                childFragmentManager.showGenreMenu()
+            }
+
+            view.startTransitionOnPreDraw()
         }
     }
 
