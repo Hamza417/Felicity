850f4b264579ca516b7f0beb19183794f86a3989
Hamza417 <23103729+Hamza417@users.noreply.github.com>
2025-09-04 09:14:52 +0530

    fixes

--- decorations/src/main/java/app/simple/felicity/decorations/views/SquareImageView.java
+++ decorations/src/main/java/app/simple/felicity/decorations/views/SquareImageView.java
@@ -9,14 +9,21 @@ import androidx.appcompat.widget.AppCompatImageView;
 public class SquareImageView extends AppCompatImageView {
     public SquareImageView(Context context) {
         super(context);
+        init();
     }
     
     public SquareImageView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
     
     public SquareImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+    
+    private void init() {
+        setClipToOutline(false);
     }
     
     @Override
