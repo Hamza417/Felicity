ae0dabd3260a655dfb521edc78bb414663752ca2
Hamza Rizwan <hamzarizwan243@gmail.com>
2026-01-29 22:58:12 +0530

    Revert "remove: manage all files permission"
    This reverts commit a1b141da48c077afd66625421fdb97dc727c5e6c.

--- decorations/src/main/java/app/simple/felicity/decorations/utils/PermissionUtils.kt
+++ decorations/src/main/java/app/simple/felicity/decorations/utils/PermissionUtils.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
+import android.os.Environment
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
@@ -34,11 +35,26 @@ object PermissionUtils {
         }
     }
 
+    fun Context.isManageExternalStoragePermissionGranted(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Environment.isExternalStorageManager()
+        } else {
+            ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
     fun Fragment.isReadMediaAudioPermissionGranted(): Boolean {
         return requireContext().isReadMediaAudioPermissionGranted()
     }
 
     fun Fragment.isPostNotificationsPermissionGranted(): Boolean {
         return requireContext().isPostNotificationsPermissionGranted()
+    }
+
+    fun Fragment.isManageExternalStoragePermissionGranted(): Boolean {
+        return requireContext().isManageExternalStoragePermissionGranted()
     }
 }
