da820f80b261adb76750029cc3656833a5200d9e
Hamza Rizwan <hamzarizwan243@gmail.com>
2026-03-07 17:15:21 +0530

    feat(lyrics): dynamic text alignment and size adjustments

--- music/src/main/java/app/simple/felicity/dialogs/lyrics/Lyrics.kt
+++ music/src/main/java/app/simple/felicity/dialogs/lyrics/Lyrics.kt
@@ -10,7 +10,9 @@ import app.simple.felicity.databinding.DialogLyricsBinding
 import app.simple.felicity.decorations.lrc.view.ModernLrcView
 import app.simple.felicity.dialogs.lyrics.AddLyrics.Companion.showAddLyrics
 import app.simple.felicity.extensions.dialogs.MediaBottomDialogFragment
+import app.simple.felicity.preferences.LyricsPreferences
 import app.simple.felicity.repository.constants.BundleConstants
+import app.simple.felicity.repository.managers.MediaManager
 import app.simple.felicity.repository.models.Audio
 import app.simple.felicity.shared.utils.ViewUtils.gone
 import app.simple.felicity.shared.utils.ViewUtils.visible
@@ -48,13 +50,16 @@ class Lyrics : MediaBottomDialogFragment(), AddLyrics.Companion.OnLyricsCreatedL
         // Allow the BottomSheet to take over (dismiss) once the user scrolls past the end of lyrics
         binding.lrcView.setParentDismissEnabled(true)
         binding.lrcView.setFadeLength(ModernLrcView.DEFAULT_FADE_LENGTH.div(2))
+        setAlignment()
+        applyTextSize()
 
         binding.title.text = audio?.title
         binding.artists.text = audio?.artist
 
         lyricsViewModel.getLrcData().observe(viewLifecycleOwner) { lrcData ->
+            binding.lrcView.reset()
+
             if (lrcData.isEmpty) {
-                binding.lrcView.reset()
                 binding.addLyrics.visible(animate = true)
             } else {
                 binding.lrcView.setLrcData(lrcData)
@@ -75,6 +80,41 @@ class Lyrics : MediaBottomDialogFragment(), AddLyrics.Companion.OnLyricsCreatedL
         lyricsViewModel.reloadLrcData()
     }
 
+    override fun onSeekChanged(seek: Long) {
+        super.onSeekChanged(seek)
+        // Only highlight if the dialog is showing lyrics for the currently playing song
+        val currentSong = MediaManager.getCurrentSong()
+        if (currentSong != null && audio != null && currentSong.id == audio!!.id) {
+            binding.lrcView.updateTime(seek + lyricsViewModel.syncOffset)
+        }
+    }
+
+    override fun onAudio(audio: Audio) {
+        super.onAudio(audio)
+        // If the song in the dialog is no longer the playing song, stop highlighting
+        // (updateTime will simply not be called when ids don't match, no action needed)
+        // so unhighlight the lyrics
+        if (this.audio != null && audio.id == this.audio!!.id) {
+            binding.lrcView.updateTime(MediaManager.getSeekPosition() + lyricsViewModel.syncOffset)
+        } else {
+            binding.lrcView.updateTime(-1)
+        }
+    }
+
+    private fun setAlignment(animate: Boolean = false) {
+        when (LyricsPreferences.getLrcAlignment()) {
+            LyricsPreferences.LEFT -> binding.lrcView.setTextAlignment(ModernLrcView.Alignment.LEFT, animate)
+            LyricsPreferences.CENTER -> binding.lrcView.setTextAlignment(ModernLrcView.Alignment.CENTER, animate)
+            LyricsPreferences.RIGHT -> binding.lrcView.setTextAlignment(ModernLrcView.Alignment.RIGHT, animate)
+        }
+    }
+
+    /** Applies the current text-size preferences to the view immediately. */
+    private fun applyTextSize() {
+        val normal = LyricsPreferences.getLrcTextSize()
+        binding.lrcView.setTextSizes(normal, normal * LRC_HIGHLIGHT_TIMES)
+    }
+
     companion object {
         fun newInstance(audio: Audio): Lyrics {
             val args = Bundle()
@@ -85,6 +125,8 @@ class Lyrics : MediaBottomDialogFragment(), AddLyrics.Companion.OnLyricsCreatedL
         }
 
         private const val TAG = "Lyrics"
+
+        private const val LRC_HIGHLIGHT_TIMES = 1.2F
 
         fun FragmentManager.showLyrics(audio: Audio) {
             val dialog = newInstance(audio)
