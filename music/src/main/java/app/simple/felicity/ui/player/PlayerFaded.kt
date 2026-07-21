9b88e82bf4aeecc82481c7c19f2964aa573b5139
Hamza Rizwan <hamzarizwan243@gmail.com>
2026-07-21 22:22:38 +0530

    add(faded): stack waveform and media controls

--- music/src/main/java/app/simple/felicity/ui/player/PlayerFaded.kt
+++ music/src/main/java/app/simple/felicity/ui/player/PlayerFaded.kt
@@ -15,6 +15,7 @@ import app.simple.felicity.decorations.views.FavoriteButton
 import app.simple.felicity.decorations.views.FelicityMediaControls
 import app.simple.felicity.decorations.views.FelicityVisualizer
 import app.simple.felicity.extensions.fragments.BasePlayerFragment
+import app.simple.felicity.preferences.UserInterfacePreferences
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -90,6 +91,18 @@ class PlayerFaded : BasePlayerFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentPlayerFadedWaveformBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.seekbarContainer.isOverlapping = UserInterfacePreferences.isStackMediaControls()
+
+        binding.seekbar.labelGravity = if (binding.seekbarContainer.isOverlapping) {
+            WaveformSeekbar.LABEL_GRAVITY_BOTTOM
+        } else {
+            WaveformSeekbar.LABEL_GRAVITY_CENTER
+        }
     }
 
     companion object {
